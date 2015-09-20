f7229043c842b4195f795c383c31c599f80b43ca
Ben Fry <fry@processing.org>
2015-09-20 11:55:13 -0400

    update based on current best practice, post 3.0 beta 6

--- src/template/tool/HelloTool.java
+++ src/template/tool/HelloTool.java
@@ -7,48 +7,52 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General
  * Public License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA  02111-1307  USA
- * 
- * @author		##author##
- * @modified	##date##
- * @version		##tool.prettyVersion##
+ *
+ * @author   ##author##
+ * @modified ##date##
+ * @version  ##tool.prettyVersion##
  */
 
- package template.tool;
- 
- import processing.app.tools.Tool;
- import processing.app.ui.Editor;
- 
- 
- public class HelloTool implements Tool {
- 
- // when creating a tool, the name of the main class which implements Tool
- // must be the same as the value defined for project.name in your build.properties
- 
- 
-	public String getMenuTitle() {
-		return "##tool.name##";
-	}
- 
-	public void init(Editor theEditor) {
-	}
- 
-	public void run() {
-	    // Fill in author.name, author.url, tool.prettyVersion and project.prettyName in
-	    // build.properties for them to be auto-replaced here.
-		System.out.println("hello Tool. ##tool.name## ##tool.prettyVersion## by ##author##");
-	}
- 
- }
+package template.tool;
+
+import processing.app.tools.Tool;
+import processing.app.ui.Editor;
 
 
+// when creating a tool, the name of the main class which implements Tool must
+// be the same as the value defined for project.name in your build.properties
 
+public class HelloTool implements Tool {
+  Base base;
+
+
+  public String getMenuTitle() {
+    return "##tool.name##";
+  }
+
+
+  public void init(Editor editor) {
+    // Store a reference to the Processing application itself
+    base = editor.getBase();
+  }
+
+
+  public void run() {
+    // Get the currently active Editor to run the Tool on it
+    Editor editor = base.getActiveEditor();
+
+    // Fill in author.name, author.url, tool.prettyVersion and
+    // project.prettyName in build.properties for them to be auto-replaced here.
+    System.out.println("Hello Tool. ##tool.name## ##tool.prettyVersion## by ##author##");
+  }
+}
